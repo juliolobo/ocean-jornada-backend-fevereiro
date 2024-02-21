679995376088bfb740803316f1d36c372c778e39
Julio Lobo <juliolobo@lobohost.com.br>
2024-02-21 15:05:49 +0000

    Adicionado EndPoint `POST`

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello, World')
 })
 
 app.get('/oi', function (req, res) {
     res.send('Olá Mundo')
+})
+
+//Lista de personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer smith']
+
+app.get('/item', function (req, res) {
+  res.send(lista)
+})
+
+app.get('/item/:id', function (req, res) {
+  const id = req.params.id
+  const item = lista[id]
+  
+  res.send(item)
+})
+
+app.use(express.json())
+
+app.post('/item', function (req, res) {
+  const body = req.body
+  const item = body.nome
+  lista.push(item)
+  
+  res.send('Item adicionado com sucesso')
 })
 
 app.listen(3000)
